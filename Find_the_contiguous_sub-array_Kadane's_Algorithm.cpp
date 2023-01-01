29c08c3837a7a3c4cfe9e25201aaa2c43b24bb86
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-01 20:57:25 +0530

    Q5

---
+++ Find_the_contiguous_sub-array_Kadane's_Algorithm.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // arr: input array
+    // n: size of array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n)
+    {
+
+        int least_min = -214742, sum = 0;
+        //   max_so_far =least_min
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (least_min < sum)
+                least_min = sum;
+
+            if (sum < 0)
+                sum = 0;
+        }
+        return least_min;
+    }
+};
+
+int main()
+{
+    int t, n;
+
+    cin >> t;   // input testcases
+    while (t--) // while testcases exist
+    {
+
+        cin >> n; // input size of array
+
+        int a[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i]; // inputting elements of array
+
+        Solution ob;
+
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// Example-->
+// Input:
+// N = 5
+// Arr[] = {1,2,3,-2,5}
+// Output:
+// 9
+// Explanation:
+// Max subarray sum is 9
+// of elements (1, 2, 3, -2, 5) which
+// is a contiguous subarray.
