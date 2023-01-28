bb72279a1db386ce08fd7a1b43479db3f5e59648
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-28 16:36:23 +0530

    28-jan-23

---
+++ Q50_split_the_binary_string_into_substring.cpp
@@ -0,0 +1,45 @@
+// Q50
+// no. 67
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxSubStr(string s)
+    {
+        int n = s.length();
+        int x = 0, y = 0, c = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '0')
+                x++;
+            else
+                y++;
+
+            if (x == y)
+                c++;
+        }
+        if (x != y)
+        {
+            return -1;
+        }
+
+        return c;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.maxSubStr(str);
+        cout << ans << endl;
+    }
+    return 0;
+}
